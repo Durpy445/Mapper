ec7f94f1e99cf830cadee90fc847e75fa5776922
Durpy445 <durp446@gmail.com>
2025-09-09 10:27:40 +0100

    Added a max amount of sites/ sorting by amount

--- Compile.py
+++ Compile.py
@@ -2,12 +2,35 @@ import json
 with open('Data.json', 'r') as file:
     data = json.load(file)
 
+Max = 100
+Amount = {}
+
+def Add(Website):
+    if Website not in Amount:
+        Amount[Website] = 1
+    else:
+        Amount[Website] += 1
+
+
+for Parent in data:
+    Add(Parent)
+    for Web in data[Parent]:
+        Add(Web)
+
+
+
+TopSites = sorted(Amount.items(), key=lambda x: (-x[1], x[0]))
+List = [site for site, i in TopSites[:Max]]
+
+
+print(List)
+
 import networkx as nx
 G = nx.Graph()
 
 
-for i in data:
-    G.add_node(i)
+for i in List:
+    G.add_node(i[0])
 
 import matplotlib.pyplot as plt 
 
